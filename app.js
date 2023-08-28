38bf9e7845cdcd562f3bfe4274d2cf08128ed3e3
diony0409@naver.com <diony0409@naver.com>
2023-08-28 17:41:39 +0900

    0828 dion end

--- app.js
+++ app.js
@@ -23,6 +23,26 @@ app.use(express.static(publicPath));
 
 const PORT = 8088;
 
+const ejs = require('ejs')
+const mysql = require('mysql')
+
+app.set('ejs', ejs.renderFile)
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: '1234',
+    database: 'productservice'
+});
+
+app.get('/',(req, res) => {
+
+    con.query('select * from title', function (err, results, fields) {
+        if (err) throw err
+        res.render('index.ejs', { content: results})
+    });
+})
+
 app.listen(PORT, function() {
     console.log('Listening on port: ', PORT);
 });
